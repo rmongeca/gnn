55e93ca580697ad669f8afd45a0895a9bfebe2a7
Ricard Monge Calvo <rikymonge@gmail.com>
2021-03-06 17:38:03 +0100

    [NEW] Update QM9 use case.

--- qm9.py
+++ qm9.py
@@ -1,35 +1,52 @@
 """Example GNN for QM9"""
-import json
+from gnn.readout import GatedReadout
+from gnn.update import GRUUpdate
 import numpy as np
 import os
 import tensorflow as tf
 from datetime import datetime
+from pathlib import Path
 from time import time
 
-from gnn import GNN, GNNInput, DenseInitializer
+from gnn import GNN, GNNInput
+from gnn.initial import DenseInitializer
+from gnn.message_passing import EdgeNetMessagePassing
 
 tf.keras.backend.clear_session()
 # tf.config.experimental_run_functions_eagerly(True)
 
 np.random.seed(42)
 
-training_dir = "/home/rmonge/UPC/TFM/gnn/data/training"
-training_fn = [os.path.join(training_dir, fn) for fn in os.listdir(training_dir)
-               if not fn.startswith(".")]
-test_dir = "/home/rmonge/UPC/TFM/gnn/data/test"
-test_fn = [os.path.join(test_dir, fn) for fn in os.listdir(test_dir) if not fn.startswith(".")]
+# Paths
+log_dir = Path("/home/rmonge/UPC/TFM/gnn/data/logs")
+test_dir = Path("/home/rmonge/UPC/TFM/gnn/data/qm9/test")
+training_dir = Path("/home/rmonge/UPC/TFM/gnn/data/qm9/training")
+
+# Constants
 target = "dipole_moment"
 
+# Files
+test_fn = [test_dir / fn for fn in os.listdir(test_dir) if not fn.startswith(".")]
+training_fn = [training_dir / fn for fn in os.listdir(training_dir) if not fn.startswith(".")]
 print(f"Training samples: {len(training_fn)}")
 print(f"Test samples: {len(test_fn)}")
 
+# Logs
+log_name = f"gnn_qm9_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+log_subdir = log_dir / log_name
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_subdir)
+
+# TF Datasets
 training = tf.data.Dataset.from_generator(
     **GNNInput.get_data_generator(training_fn, target))
 test = tf.data.Dataset.from_generator(
     **GNNInput.get_data_generator(test_fn, target))
 
-model = GNN(hidden_state_size=15, message_size=20, message_passing_iterations=3, output_size=1)
-
+# Training params
+n_epochs = 5
+batch_size = 32
+train_step_per_epochs = len(training_fn) // batch_size + 1
+valid_step_per_epoch = len(test_fn)
 learning_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
     initial_learning_rate=1.935e-4, decay_steps=900, end_learning_rate=1.84e-4, power=1.0
 )
@@ -37,21 +54,22 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=learning_schedule)
 loss = tf.keras.losses.MeanSquaredError()
 metrics = [tf.keras.metrics.MeanAbsoluteError()]
 
+
+# GNN Model
+model = GNN(hidden_state_size=15, message_size=20, message_passing_iterations=3,
+            output_size=len(target), initializer=DenseInitializer,
+            message_passing=EdgeNetMessagePassing, update=GRUUpdate, readout=GatedReadout)
 model.compile(
     optimizer=optimizer,
     loss=loss,
     metrics=metrics
 )
 
-log_name = f"gnn_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
-log_dir = f"/home/rmonge/UPC/TFM/ignnition_data/CheckPoint/{log_name}"
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-
+# Model fit
 start = time()
 loss = model.fit(
-    training.batch(1), epochs=10, steps_per_epoch=1000,
-    validation_data=test.batch(1), validation_freq=1, validation_steps=1000,
+    training.padded_batch(batch_size), epochs=n_epochs, steps_per_epoch=train_step_per_epochs,
+    validation_data=test.padded_batch(1), validation_freq=1, validation_steps=valid_step_per_epoch,
     callbacks=[tensorboard_callback], use_multiprocessing=True)
 elapsed = time() - start
 print(f"Fit ended after {elapsed} seconds")
-
