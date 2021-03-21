0d01f1beb3dac02be59c2e1ba59184d9e06d2fca
Ricard Monge Calvo <rikymonge@gmail.com>
2021-03-21 19:28:45 +0100

    [NEW] Add Gated Readout MLP output network.

--- gnn/readout.py
+++ gnn/readout.py
@@ -3,6 +3,7 @@ import tensorflow as _tf
 from abc import ABC as _ABC, abstractmethod as _abstractmethod
 
 from .input import ReadoutInput
+from .mlp import MLP
 
 
 class ReadoutLayer(_tf.keras.layers.Layer, _ABC):
@@ -26,37 +27,55 @@ class ReadoutLayer(_tf.keras.layers.Layer, _ABC):
             hidden=inputs.hidden[batch, :, :],
             hidden_initial=inputs.hidden_initial[batch, :, :],
         )
-        return self.readout(batch_inputs, training=training)
+        return self._readout(batch_inputs, training=training)
+
+    @_tf.function
+    def _readout(self, inputs: ReadoutInput, training=None):
+        """Wrapper around readout method with tf.function decorator."""
+        return self.readout(inputs=inputs, training=training)
 
     @_abstractmethod
     def readout(self, inputs: ReadoutInput, training=None):
+        """Readout method to apply to a certain batch's ReadoutInput."""
         pass
 
 
 class GatedReadout(ReadoutLayer):
     """Readout function layer with Gated NN for GNN model."""
 
-    def __init__(self, hidden_state_size=10, output_size=1,
-                 gate_activation=_tf.keras.activations.sigmoid, *args, **kwargs):
+    def __init__(
+        self, hidden_state_size=10, output_size=1, nn_layers=3, nn_units=50, nn_activation="relu",
+        gate_activation=_tf.keras.activations.sigmoid, *args, **kwargs
+    ):
         super(GatedReadout, self).__init__(*args, **kwargs)
         self.hidden_state_size = hidden_state_size
         self.output_size = output_size
-        self.gate_activation = gate_activation
+        self.nn_units = nn_units
         # Init inner layers
-        self.gate = _tf.keras.layers.Dense(units=output_size, name="readout-gate-hidden-initial")
-        self.state = _tf.keras.layers.Dense(units=output_size, name="readout-state-hidden")
+        self.gate = MLP(
+            activation=nn_activation, name="readout-gate", num_layers=nn_layers, units=nn_units,
+            output_activation=gate_activation, **kwargs)
+        self.state = MLP(
+            activation=nn_activation, name="readout-state", num_layers=nn_layers, units=nn_units,
+            **kwargs)
+        self.graph = MLP(
+            activation=nn_activation, name="readout-state", num_layers=nn_layers, units=nn_units,
+            output_units=output_size, **kwargs)
 
     def build(self, input_shapes):
-        gate_shape = _tf.TensorShape([None, None, input_shapes.hidden[2]*2])
+        gate_shape = _tf.TensorShape([None, input_shapes.hidden[2]*2])
+        state_shape = _tf.TensorShape([None, input_shapes.hidden[2]])
+        graph_shape = _tf.TensorShape([None, self.nn_units])
         self.gate.build(gate_shape)
-        self.state.build(input_shapes.hidden)
+        self.state.build(state_shape)
+        self.graph.build(graph_shape)
         super(ReadoutLayer, self).build([])
 
     def readout(self, inputs: ReadoutInput, training=None):
         hidden = inputs.hidden
         hidden_initial = inputs.hidden_initial
         hidden_concat = _tf.concat([hidden, hidden_initial], axis=-1)
-        gate_in = self.gate(hidden_concat, training=training)
-        gate = self.gate_activation(gate_in)
+        gate = self.gate(hidden_concat, training=training)
         state = self.state(hidden, training=training)
-        return _tf.math.reduce_sum(_tf.math.multiply(gate, state), axis=0)
+        reduced = _tf.math.reduce_sum(_tf.math.multiply(gate, state), axis=0, keepdims=True)
+        return _tf.squeeze(self.graph(reduced), axis=0)
