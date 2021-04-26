72491d7153d5cd21933cd386503ef08d380d49af
Ricard Monge Calvo <rikymonge@gmail.com>
2021-04-27 01:02:35 +0200

    [REF] Update QM9 training options and settings.

--- qm9.py
+++ qm9.py
@@ -20,15 +20,25 @@ np.random.seed(42)
 
 # Paths
 log_dir = Path("data/logs")
-validation_dir = Path("data/qm9/validation")
-training_dir = Path("data/qm9/training")
+training_dir = Path("data/qm9/raw/train")
+validation_dir = Path("data/qm9/raw/validation")
 
 # Constants
 node_feature_names = [
-    "element_c", "element_f", "element_h", "element_n", "element_o",
-    "aromatic", "acceptor", "donor", "atomic_number",
-    "hybridization_null", "hybridization_sp", "hybridization_sp2",
-    "hybridization_sp3", "hydrogen_count"
+    "acceptor",
+    "aromatic",
+    "atomic_number",
+    "donor",
+    "element_c",
+    "element_f",
+    "element_h",
+    "element_n",
+    "element_o",
+    "hybridization_null",
+    "hybridization_sp",
+    "hybridization_sp2",
+    "hybridization_sp3",
+    "hydrogen_count",
 ]
 edge_feature_names = [
     "distance", "order_1", "order_1_5", "order_2", "order_3"
@@ -36,15 +46,18 @@ edge_feature_names = [
 target = "dipole_moment"
 
 # Files
-validation_fn = [validation_dir / fn for fn in os.listdir(validation_dir) if not fn.startswith(".")]
 training_fn = [training_dir / fn for fn in os.listdir(training_dir) if not fn.startswith(".")]
+validation_fn = [validation_dir / fn for fn in os.listdir(validation_dir) if not fn.startswith(".")]
 print(f"Training samples: {len(training_fn)}")
 print(f"Validation samples: {len(validation_fn)}")
 
 # Logs
 log_name = f"gnn_qm9_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
 log_subdir = log_dir / log_name
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_subdir)
+callbacks = [
+    tf.keras.callbacks.TensorBoard(
+        log_dir=log_subdir, update_freq='epoch', write_images=False, histogram_freq=1)
+]
 
 # TF Datasets
 training = tf.data.Dataset.from_generator(
@@ -53,22 +66,22 @@ validation = tf.data.Dataset.from_generator(
     **GNNInput.get_data_generator(validation_fn, node_feature_names, edge_feature_names, target))
 
 # GNN Model
-model = GNN(hidden_state_size=20, message_size=45, message_passing_iterations=3,
+model = GNN(hidden_state_size=25, message_size=25, message_passing_iterations=4,
             output_size=1, initializer=PadInitializer, message_passing=FeedForwardMessage,
             update=GRUUpdate, readout=GatedReadout)
 
 # Training params
-batch_size = 2
-n_epochs = 1
-train = training.padded_batch(batch_size)
-train_step_per_epochs = len(training_fn) // batch_size
-valid = validation.padded_batch(1)
-valid_step_per_epoch = len(validation_fn)
+batch_size = 1
+n_epochs = 20
+train = training.batch(batch_size)
+train_step_per_epochs = 1000
+valid = validation.batch(1)
+valid_step_per_epoch = 100
 validation_freq = 1
 
 # Model compile parameters
 learning_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
-    initial_learning_rate=1.935e-4, decay_steps=n_epochs, end_learning_rate=1.84e-4, power=1.0
+    initial_learning_rate=1.935e-4, decay_steps=20000, end_learning_rate=1.84e-4, power=1.0
 )
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_schedule)
 loss = tf.keras.losses.MeanSquaredError()
@@ -85,6 +98,6 @@ start = time()
 loss = model.fit(
     train, epochs=n_epochs, steps_per_epoch=train_step_per_epochs,
     validation_data=valid, validation_freq=validation_freq, validation_steps=valid_step_per_epoch,
-    callbacks=[tensorboard_callback], use_multiprocessing=True)
+    callbacks=callbacks, use_multiprocessing=True)
 elapsed = time() - start
 print(f"Fit ended after {elapsed} seconds")
