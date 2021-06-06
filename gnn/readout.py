5b8fbaf0f6af2e1b043300d5858304a9418ce0b8
Ricard Monge <rikymonge@gmail.com>
2021-06-07 00:48:13 +0200

    [REF] Rethink graph/node level readout base layers.

--- gnn/readout.py
+++ gnn/readout.py
@@ -6,13 +6,53 @@ from .input import ReadoutInput
 from .mlp import MLP
 
 
-class ReadoutLayer(_tf.keras.layers.Layer, _ABC):
-    """Base Readout Layer abstract class to be inherited by Readout Layer implementations.
+class ReadoutLocalLayer(_tf.keras.layers.Layer, _ABC):
+    """Base Readout Local Layer abstract class to be inherited by Readout Layer implementations.
 
-    Abstract class to define handling of Batched ReadoutInput for readout layer to perform Readout.
-    Child classes must implement the readout method, which takes as argument a non-batched
-    ReadoutInput.
+    Abstract class to define handling of Batched ReadoutInput for readout layer to perform Local or
+    node-level Readout. Child classes must implement the readout method, which takes as argument a
+    non-batched ReadoutInput and return a Tensor of shape (None, output_size) where None represents
+    the number of nodes.
     """
+
+    def __init__(self, hidden_state_size=10, output_size=1, *args, **kwargs):
+        """Constructor for Readout Layer abstract class."""
+        super(ReadoutLocalLayer, self).__init__(*args, **kwargs)
+        self.output_size = output_size
+
+    def call(self, inputs: ReadoutInput, training=None):
+        batch_size = _tf.shape(inputs.hidden)[0]
+        return _tf.map_fn(
+            fn=lambda batch: self._handle_batch(batch, inputs, training=training),
+            elems=_tf.range(batch_size),
+            fn_output_signature=_tf.TensorSpec([None, self.output_size], float)
+        )
+
+    def _handle_batch(self, batch, inputs: ReadoutInput, training=None):
+        batch_inputs = ReadoutInput(
+            hidden=inputs.hidden[batch, :, :],
+            hidden_initial=inputs.hidden_initial[batch, :, :],
+        )
+        return self.readout(batch_inputs, training=training)
+
+    @_abstractmethod
+    def readout(self, inputs: ReadoutInput, training=None):
+        """Readout method to apply to a certain batch's ReadoutInput."""
+        pass
+
+
+class ReadoutGlobalLayer(_tf.keras.layers.Layer, _ABC):
+    """Base Readout Global Layer abstract class to be inherited by Readout Layer implementations.
+
+    Abstract class to define handling of Batched ReadoutInput for readout layer to perform Global or
+    graph-level Readout. Child classes must implement the readout method, which takes as argument a
+    non-batched ReadoutInput and return a Tensor of shape (output_size,).
+    """
+
+    def __init__(self, hidden_state_size=10, output_size=1, *args, **kwargs):
+        """Constructor for Readout Layer abstract class."""
+        super(ReadoutGlobalLayer, self).__init__(*args, **kwargs)
+        self.output_size = output_size
 
     def call(self, inputs: ReadoutInput, training=None):
         batch_size = _tf.shape(inputs.hidden)[0]
@@ -35,36 +75,57 @@ class ReadoutLayer(_tf.keras.layers.Layer, _ABC):
         pass
 
 
-class GatedReadout(ReadoutLayer):
+class FeedForwardReadout(ReadoutLocalLayer):
+    """Readout function layer with a Feed Forward NN for GNN model."""
+
+    def __init__(
+        self, output_size=1, activation="relu", layer=None, num_layers=4, output_activation=None,
+        units=50, *args, **kwargs
+    ):
+        super(FeedForwardReadout, self).__init__(output_size=output_size, *args, **kwargs)
+        self.mlp = MLP(
+            activation=activation, layer=layer, name="readout-ff-net", num_layers=num_layers,
+            output_activation=output_activation, output_units=output_size, units=units, **kwargs)
+
+    def build(self, input_shapes):
+        self.mlp.build(_tf.TensorShape([None, input_shapes.hidden[2]]))
+        super(FeedForwardReadout, self).build([])
+
+    def readout(self, inputs: ReadoutInput, training=None):
+        hidden = inputs.hidden
+        return self.mlp(hidden, training=training)
+
+
+class GatedReadout(ReadoutGlobalLayer):
     """Readout function layer with Gated NN for GNN model."""
 
     def __init__(
-        self, hidden_state_size=10, output_size=1, nn_layers=3, nn_units=50, nn_activation="relu",
-        gate_activation=_tf.keras.activations.sigmoid, *args, **kwargs
+        self, hidden_state_size=10, output_size=1, activation="relu", layer=None, num_layers=3,
+        gate_activation=_tf.keras.activations.sigmoid, units=50, *args, **kwargs
     ):
-        super(GatedReadout, self).__init__(*args, **kwargs)
+        super(GatedReadout, self).__init__(output_size=output_size, *args, **kwargs)
         self.hidden_state_size = hidden_state_size
         self.output_size = output_size
-        self.nn_units = nn_units
+        self.units = units
         # Init inner layers
         self.gate = MLP(
-            activation=nn_activation, name="readout-gate", num_layers=nn_layers, units=nn_units,
-            output_activation=gate_activation, **kwargs)
+            activation=activation, layer=layer, name="readout-gate", num_layers=num_layers,
+            output_activation=gate_activation, units=units, **kwargs)
         self.state = MLP(
-            activation=nn_activation, name="readout-state", num_layers=nn_layers, units=nn_units,
-            **kwargs)
+            activation=activation, layer=layer, name="readout-state", num_layers=num_layers,
+            units=units, **kwargs)
         self.graph = MLP(
-            activation=nn_activation, name="readout-state", num_layers=nn_layers, units=nn_units,
-            output_units=output_size, **kwargs)
+            activation=activation, layer=layer, name="readout-state", num_layers=num_layers,
+            output_units=output_size, units=units, **kwargs)
 
     def build(self, input_shapes):
         gate_shape = _tf.TensorShape([None, input_shapes.hidden[2]*2])
         state_shape = _tf.TensorShape([None, input_shapes.hidden[2]])
-        graph_shape = _tf.TensorShape([None, self.nn_units])
+        graph_shape = _tf.TensorShape([None, self.units])
         self.gate.build(gate_shape)
         self.state.build(state_shape)
         self.graph.build(graph_shape)
-        super(ReadoutLayer, self).build([])
+        super(GatedReadout, self).build([])
 
     def readout(self, inputs: ReadoutInput, training=None):
         hidden = inputs.hidden
