39ff972594d42d907a58a9871930ffbf340bb4ef
Ricard Monge <rikymonge@gmail.com>
2021-06-07 00:47:23 +0200

    [NEW] Update MLP sequential model for more flexibility.

--- gnn/mlp.py
+++ gnn/mlp.py
@@ -3,16 +3,40 @@ import tensorflow as _tf
 
 
 def MLP(
-    activation="relu", layer=None, name="mlp", num_layers=2, output_units=None,
-    output_activation=None, units=50, **kwargs
+    activation="relu", layer=None, name="mlp", num_layers=2, output_activation=None,
+    output_layer=None, output_units=None, units=50, **kwargs
 ):
     """Create a (Sequential) Multi Layer Perceptron with dense layers."""
+    assert isinstance(num_layers, int) and num_layers > 0
     if layer is None:
         layer = _tf.keras.layers.Dense
+
+    # If layer is a string, repeat for each non-output layer
+    if not isinstance(layer, list):
+        layer = [layer] * (num_layers - 1) if num_layers > 1 else [layer]
+    if output_layer is None:
+        output_layer = layer[-1]
+
+    # If activation is a string, repeat for each non-output layer
+    if not isinstance(activation, list):
+        activation = [activation] * (num_layers - 1) if num_layers > 1 else [activation]
+
+    # If units is a number, repeat for each non-output layer
+    if not isinstance(units, list):
+        units = [units] * (num_layers - 1) if num_layers > 1 else [units]
     if output_units is None:
-        output_units = units
+        output_units = units[-1]
+
     model = _tf.keras.Sequential(name=name)
-    for i in range(1, num_layers):
-        model.add(layer(units=units, activation=activation, name=f"{name}-dense-{i}", **kwargs))
-    model.add(layer(units=output_units, activation=output_activation, name=f"{name}-output"))
+    for i in range(num_layers-1):
+        model.add(
+            layer[i](
+                units=units[i], activation=activation[i], name=f"{name}-dense-{i}", **kwargs
+            )
+        )
+    model.add(
+        output_layer(units=output_units, activation=output_activation, name=f"{name}-output")
+        if output_layer != _tf.keras.layers.Activation else
+        output_layer(activation=output_activation, name=f"{name}-output")
+    )
     return model
