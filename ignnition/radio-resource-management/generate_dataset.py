9d70508a4a153baabf723b5e7b62a3751d14faba
Ricard Monge <rikymonge@gmail.com>
2021-06-12 01:13:35 +0200

    [NEW] Update radio resources management dataset generator.

--- ignnition/radio-resource-management/generate_dataset.py
+++ ignnition/radio-resource-management/generate_dataset.py
@@ -10,18 +10,16 @@ References:
    vol. 13, no. 5, pp. 2809-2823, May 2014, doi: 10.1109/TWC.2014.040214.131770.
 """
 import json
-import math
 import networkx as nx
 import numpy as np
 import os
-import shutil
 from itertools import product
 from pathlib import Path
 from tqdm.auto import tqdm
 
 # Generation options
 empty_dirs = True
-random_seed = 20210205
+random_seed = 20210516
 root_path = Path(__file__).parent
 raw_dir = root_path / Path("data/raw")
 train_dir = root_path / Path("data/train")
@@ -33,21 +31,27 @@ total_samples = train_samples + validation_samples
 rng = np.random.default_rng(seed=random_seed)
 
 # Dataset options, please see the referenced papers for more details
-n_links = 10
+n_links = 50
 field_length = 1000
 shortest_directLink_length = 2
 longest_directLink_length = 65
 shortest_crossLink_length = 1
+bandwidth = 5e6
 carrier_f = 2.4e9
 tx_height = 1.5
 rx_height = 1.5
-# Channel loss options
+antenna_gain_decibel = 2.5
+tx_power_milli_decibel = 40
+tx_power = np.power(10, (tx_power_milli_decibel - 30) / 10)
+noise_density_milli_decibel = -169
+input_noise_power = np.power(10, ((noise_density_milli_decibel - 30) / 10)) * bandwidth
+output_noise_power = input_noise_power
+noise_power = output_noise_power / tx_power
 signal_lambda = 2.998e8 / carrier_f
 Rbp = 4 * tx_height * rx_height / signal_lambda
-Lbp = abs(20 * np.log10(np.power(signal_lambda, 2) / (8 * np.pi * tx_height * rx_height)))
-antenna_gain_decibel = 2.5
-# Network additional inputs
-noise_power = 4e-12
+Lbp = abs(
+    20 * np.log10(np.power(signal_lambda, 2) / (8 * np.pi * tx_height * rx_height))
+)
 
 
 def _empty_dirs(dirs=None):
@@ -78,8 +82,8 @@ def compute_losses(distances, add_shadowing=True, add_fast_fading=True):
         channel_losses = channel_losses * np.power(10.0, shadow_coefficients / 10)
     if add_fast_fading:
         fast_fadings = (
-            np.power(rng.normal(loc=0, scale=1, size=np.shape(channel_losses)), 2) +
             np.power(rng.normal(loc=0, scale=1, size=np.shape(channel_losses)), 2)
+            + np.power(rng.normal(loc=0, scale=1, size=np.shape(channel_losses)), 2)
         ) / 2
         channel_losses = channel_losses * fast_fadings
     # Join non-diagonal path with diagonal channel losses
@@ -110,10 +114,7 @@ def generate_layout():
                 pair_angles = rng.uniform(low=0, high=np.pi * 2)
                 rx_x = tx_xs[i] + pair_dist * np.cos(pair_angles)
                 rx_y = tx_ys[i] + pair_dist * np.sin(pair_angles)
-                if (
-                    0 <= rx_x <= field_length
-                    and 0 <= rx_y <= field_length
-                ):
+                if 0 <= rx_x <= field_length and 0 <= rx_y <= field_length:
                     got_valid_rx = True
             rx_xs.append(rx_x)
             rx_ys.append(rx_y)
@@ -150,37 +151,45 @@ def generate_graphs(output_dir="data/raw", output_prefix="network", empty_dirs=F
         path_loss, channel_loss = compute_losses(dist)
         assert np.shape(layout) == (N, 4)
         assert np.shape(dist) == np.shape(path_loss) == np.shape(channel_loss) == (N, N)
-        diag_dist = np.diag(1/dist)
-        diag_channel_loss = np.diag(channel_loss)
-        adjacency = channel_loss - np.multiply(np.eye(N), channel_loss)  # Remove own pair
+        dist_inv = 1 / dist
+        channel_loss_sqrt = np.sqrt(channel_loss)
+        diag_dist = np.diag(dist_inv)
+        diag_channel_loss = np.diag(channel_loss_sqrt)
+        adjacency = dist_inv - np.multiply(np.eye(N), dist_inv)  # Remove own pair
         weights = rng.uniform(size=N)  # Transceiver-receiver random weights
         weights = weights / weights.sum()  # Normalize weights
-        wmmse_power = get_wmmse_power(channel_loss, noise_power)
+        wmmse_power = get_wmmse_power(channel_loss_sqrt, weights, noise_power)
         graph = nx.DiGraph()
         # We add as node attribute a placeholder per node power label to use as target
         # altough the loss we plan to use will be unsupervised.
-        graph.add_nodes_from([
-            (link_idx, {
-                "entity": "transmitter_receiver_pair",
-                "transceiver_x": layout[link_idx, 0],
-                "transceiver_y": layout[link_idx, 1],
-                "receiver_x": layout[link_idx, 2],
-                "receiver_y": layout[link_idx, 3],
-                "receiver_distance": diag_dist[link_idx],
-                "channel_loss": diag_channel_loss[link_idx],
-                "path_loss": path_loss[:, link_idx].tolist(),
-                "power": 0,
-                "weights": weights[link_idx],
-                "wmmse_power": wmmse_power[link_idx],
-            })
-            for link_idx in range(N)
-        ])
-        graph.add_edges_from([
-            (src, dst, {
-                "transceiver_receiver_loss": adjacency[src, dst]
-            })
-            for src, dst in product(range(N), range(N)) if src != dst
-        ])
+        graph.add_nodes_from(
+            [
+                (
+                    link_idx,
+                    {
+                        "entity": "transmitter_receiver_pair",
+                        "transceiver_x": layout[link_idx, 0],
+                        "transceiver_y": layout[link_idx, 1],
+                        "receiver_x": layout[link_idx, 2],
+                        "receiver_y": layout[link_idx, 3],
+                        "receiver_distance": diag_dist[link_idx],
+                        "channel_loss": diag_channel_loss[link_idx],
+                        "path_loss": path_loss[:, link_idx].tolist(),
+                        "power": 0,
+                        "weights": weights[link_idx],
+                        "wmmse_power": wmmse_power[link_idx],
+                    },
+                )
+                for link_idx in range(N)
+            ]
+        )
+        graph.add_edges_from(
+            [
+                (src, dst, {"transceiver_receiver_loss": adjacency[src, dst]})
+                for src, dst in product(range(N), range(N))
+                if src != dst
+            ]
+        )
         graph.graph["noise_power"] = noise_power
         filepath = Path(output_dir) / f"{output_prefix}_{i}.json"
         with filepath.open("w") as _f:
@@ -188,40 +197,56 @@ def generate_graphs(output_dir="data/raw", output_prefix="network", empty_dirs=F
     print(f"Finished generating {total_samples} network graphs in {output_dir}.")
 
 
-def get_wmmse_power(channel_loss, noise_power, max_iterations=100):
+def get_wmmse_power(
+    channel_loss, weights, noise_power, max_iterations=100, n_repeats=5
+):
     """Get WMMSE optimimum power aproximation for given matrix of channel losses and noise power."""
-    H = channel_loss
+    N = n_repeats
     K = np.shape(channel_loss)[0]
-    P_ini = np.random.rand(K, 1)
-    P_max = np.ones([K, 1])
-    vnew = 0
-    b = np.sqrt(P_ini)
-    f = np.zeros(K)
-    w = np.zeros(K)
-    for i in range(K):
-        f[i] = H[i, i] * b[i] / (np.matmul(np.square(H[i, :]), np.square(b)) + noise_power)
-        w[i] = 1 / (1 - f[i] * b[i] * H[i, i])
-        vnew += math.log2(w[i])
-
+    H = np.tile(np.expand_dims(channel_loss, axis=0), [N, 1, 1])
+    Pmax = 1
+    Pini = np.random.rand(N, K, 1)
+    b = np.sqrt(Pini)
+    f = np.zeros((N, K, 1))
+    w = np.zeros((N, K, 1))
+    # Init f & w values for next iteratin
+    mask = np.eye(K)
+    rx_power = np.multiply(H, b)
+    rx_power_s = np.square(rx_power)
+    valid_rx_power = np.sum(np.multiply(rx_power, mask), 1)
+    interference = np.sum(rx_power_s, 2) + noise_power
+    f = np.divide(valid_rx_power, interference)
+    w = 1 / (1 - np.multiply(f, valid_rx_power))
     for _ in range(max_iterations):
-        vold = vnew
-        for i in range(K):
-            btmp = w[i] * f[i] * H[i, i] / sum(w * np.square(f) * np.square(H[:, i]))
-            b[i] = min(btmp, np.sqrt(P_max)[i]) + max(btmp, 0) - btmp
+        # Iterate using f & w
+        fp = np.expand_dims(f, 1)
+        rx_power = np.multiply(H.transpose(0, 2, 1), fp)
+        valid_rx_power = np.sum(np.multiply(rx_power, mask), 1)
+        bup = np.multiply(weights, np.multiply(w, valid_rx_power))
+        rx_power_s = np.square(rx_power)
+        wp = np.expand_dims(w, 1)
+        weightsp = np.expand_dims(weights, 1)
+        bdown = np.sum(np.multiply(weightsp, np.multiply(rx_power_s, wp)), 2)
+        btmp = bup / bdown
+        b = (
+            np.minimum(btmp, np.ones((N, K)) * np.sqrt(Pmax))
+            + np.maximum(btmp, np.zeros((N, K)))
+            - btmp
+        )
+        # Update f & w values for next iteratin
+        bp = np.expand_dims(b, 1)
+        rx_power = np.multiply(H, bp)
+        rx_power_s = np.square(rx_power)
+        valid_rx_power = np.sum(np.multiply(rx_power, mask), 1)
+        interference = np.sum(rx_power_s, 2) + noise_power
+        f = np.divide(valid_rx_power, interference)
+        w = 1 / (1 - np.multiply(f, valid_rx_power))
+    return np.round(np.mean(np.square(b), axis=0), 10)
 
-        vnew = 0
-        for i in range(K):
-            f[i] = H[i, i] * b[i] / (np.matmul(np.square(H[i, :]), np.square(b)) + noise_power)
-            w[i] = 1 / (1 - f[i] * b[i] * H[i, i])
-            vnew += math.log2(w[i])
 
-        if vnew - vold <= 1e-3:
-            break
-
-    return np.round(np.squeeze(np.square(b)), 10)
-
-
-def join_graphs_into_dataset(files, output_dir, output_file_name="data.json", empty_dirs=False):
+def join_graphs_into_dataset(
+    files, output_dir, output_file_name="data.json", empty_dirs=False
+):
     if empty_dirs:
         _empty_dirs(output_dir)
     graphs = [json.load(open(file, "r")) for file in files]
@@ -230,7 +255,12 @@ def join_graphs_into_dataset(files, output_dir, output_file_name="data.json", em
 
 
 def split_traing_validation(
-    raw_dir, train_dir, validation_dir, train_samples, validation_samples, empty_dirs=False
+    raw_dir,
+    train_dir,
+    validation_dir,
+    train_samples,
+    validation_samples,
+    empty_dirs=False,
 ):
     if empty_dirs:
         _empty_dirs([train_dir, validation_dir])
@@ -239,22 +269,20 @@ def split_traing_validation(
         files.shape[0] >= train_samples + validation_samples
     ), "Train + Validation samples exceed number of files available."
     rng.shuffle(files)
-    training_files = files[validation_samples:(train_samples + validation_samples)]
+    training_files = files[validation_samples: (train_samples + validation_samples)]
     validation_files = files[:validation_samples]
-    print(f"Copying training graphs into {raw_dir / 'traing'}")
-    for file in training_files:
-        shutil.copy(file, raw_dir / "train")
     print(f"Joining training graphs into {train_dir}")
     join_graphs_into_dataset(training_files, output_dir=train_dir)
-    print(f"Copying validation graphs into {raw_dir / 'validation'}")
-    for file in validation_files:
-        shutil.copy(file, raw_dir / "validation")
     print(f"Joining validation graphs into {validation_dir}")
     join_graphs_into_dataset(validation_files, output_dir=validation_dir)
 
 
 if __name__ == "__main__":
-    for _dir in [raw_dir, train_dir, validation_dir, raw_dir / "train", raw_dir / "validation"]:
+    for _dir in [
+        raw_dir,
+        train_dir,
+        validation_dir,
+    ]:
         os.makedirs(_dir, exist_ok=True)
     generate_graphs(output_dir=raw_dir, empty_dirs=empty_dirs)
     split_traing_validation(
